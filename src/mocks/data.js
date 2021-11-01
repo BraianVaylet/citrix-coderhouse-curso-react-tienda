06bf2426d12d5f568457cfcd5c5ea238b40c9c3b
Braian Vaylet <braianvaylet@gmail.com>
2021-10-31 21:59:52 -0300

    Merge branch 'feature/Desafio-05'

---
+++ src/mocks/data.js
@@ -0,0 +1,163 @@
+import limonEureka from './images/lemon-eureka.png';
+import limonGenova from './images/lemon-genova.jpg';
+import limonLima from './images/lemon-lima.png';
+import limonLisbon from './images/lemon-lisbon.png';
+import limonMayer from './images/lemon-meller.jpg';
+import limonVerdelli from './images/lemon-verdelli.jpg';
+import mandarinaOrri from './images/mandarina-orri.jpg';
+import mandarinaSatsuma from './images/mandarina-satsuma.png';
+import mandarinaTango from './images/mandarina-tango.jpg';
+import naranjaLisa from './images/naranja-lisa.png';
+import naranjaOmbligo from './images/naranja-ombligo.png';
+import naranjaSangre from './images/naranja-sangre.jpg';
+import pomeloBlanco from './images/pomelo-blanco.jpg';
+import pomeloChino from './images/pomelo-chino.jpg';
+import pomeloRosa from './images/pomelo-rosa.jpg';
+import pomeloVerde from './images/pomelo-verde.jpg';
+
+export const ITEMS = [
+	{
+		id: '1',
+		title: 'Limon Eureka',
+		description: 'Esto es un limon eureka',
+		category: 'limon',
+		img: limonEureka,
+		price: 100,
+		stock: 10,
+	},
+	{
+		id: '2',
+		title: 'Limon Genova',
+		description: 'Esto es un limon genova',
+		category: 'limon',
+		img: limonGenova,
+		price: 120,
+		stock: 10,
+	},
+	{
+		id: '3',
+		title: 'Lima',
+		description: 'Esto es una lima',
+		category: 'limon',
+		img: limonLima,
+		price: 150,
+		stock: 10,
+	},
+	{
+		id: '4',
+		title: 'Limon Lisbon',
+		description: 'Este es un limon lisbon',
+		category: 'limon',
+		img: limonLisbon,
+		price: 200,
+		stock: 10,
+	},
+	{
+		id: '5',
+		title: 'Limon Mayer',
+		description: 'Este es un limon mayer',
+		category: 'limon',
+		img: limonMayer,
+		price: 90,
+		stock: 10,
+	},
+	{
+		id: '6',
+		title: 'Limon Verdelli',
+		description: 'Este es un limon verdelli',
+		category: 'limon',
+		img: limonVerdelli,
+		price: 180,
+		stock: 10,
+	},
+	{
+		id: '7',
+		title: 'Mandarina Orri',
+		description: 'Esta es una mandarina orri',
+		category: 'mandarina',
+		img: mandarinaOrri,
+		price: 80,
+		stock: 10,
+	},
+	{
+		id: '8',
+		title: 'Mandarina Satsuma',
+		description: 'Esta es una mandarina satsuma',
+		category: 'mandarina',
+		img: mandarinaSatsuma,
+		price: 90,
+		stock: 10,
+	},
+	{
+		id: '7',
+		title: 'Mandarina Tango',
+		description: 'Esta es una mandarina tango',
+		category: 'mandarina',
+		img: mandarinaTango,
+		price: 75,
+		stock: 10,
+	},
+	{
+		id: '8',
+		title: 'Naranja Lisa',
+		description: 'Esta es una naranja lisa',
+		category: 'naranja',
+		img: naranjaLisa,
+		price: 110,
+		stock: 10,
+	},
+	{
+		id: '9',
+		title: 'Naranja de Ombligo',
+		description: 'Esta es una naranja ombligo',
+		category: 'naranja',
+		img: naranjaOmbligo,
+		price: 130,
+		stock: 10,
+	},
+	{
+		id: '10',
+		title: 'Naranja Sangre',
+		description: 'Esta es una naranja sangre',
+		category: 'naranja',
+		img: naranjaSangre,
+		price: 210,
+		stock: 10,
+	},
+	{
+		id: '11',
+		title: 'Pomelo Blanco',
+		description: 'Esto es un pomelo blanco',
+		category: 'pomelo',
+		img: pomeloBlanco,
+		price: 150,
+		stock: 10,
+	},
+	{
+		id: '12',
+		title: 'Pomelo Chino',
+		description: 'Esto es un pomelo chino',
+		category: 'pomelo',
+		img: pomeloChino,
+		price: 130,
+		stock: 10,
+	},
+	{
+		id: '13',
+		title: 'Pomelo Rosa',
+		description: 'Esto es un pomelo rosa',
+		category: 'pomelo',
+		img: pomeloRosa,
+		price: 200,
+		stock: 10,
+	},
+	{
+		id: '14',
+		title: 'Pomelo Verde',
+		description: 'Esto es un pomelo verde',
+		category: 'pomelo',
+		img: pomeloVerde,
+		price: 170,
+		stock: 10,
+	},
+];
