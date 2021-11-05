b7900d38d48b4567969d773af4654881a10d48b5
Braian Vaylet <braianvaylet@gmail.com>
2021-11-05 19:54:30 -0300

    desafio 10

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {Link} from 'react-router-dom';
 import {Cart} from 'grommet-icons';
 import {Anchor, Box, Text} from 'grommet';
+import {CartContext} from 'context/CartContext';
 import styles from './CartWidgetStyles.module.css';
 
-const CartWidget = () => (
-	<Anchor
-		href="/cart"
-		label={
-			<Box
-				direction="row-responsive"
-				align="center"
-				className={styles['navbar-items']}
-				height="xxsmall"
-				width="xxsmall"
-				pad="10px"
-			>
-				<Cart color="plain" />
-				<Text pad="0 10px">4</Text>
-			</Box>
-		}
-	/>
-);
+const CartWidget = () => {
+	const {total} = useContext(CartContext);
+
+	return total !== 0 && (
+		<Anchor
+			as={Link}
+			to="/cart"
+			label={
+				<Box
+					direction="row-responsive"
+					align="center"
+					className={styles['navbar-items']}
+					height="xxsmall"
+					width="xxsmall"
+					pad="10px"
+				>
+					<Cart color="plain" />
+					<Text>
+						({total})
+					</Text>
+				</Box>
+			}
+		/>
+	);
+};
 
 export default CartWidget;
