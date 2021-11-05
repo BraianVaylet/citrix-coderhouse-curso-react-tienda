bf070943344f2bdf5f0e949161fa3f18a896f08f
Braian Vaylet <braianvaylet@gmail.com>
2021-11-05 12:47:04 -0300

    desafio 8

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import Product from 'pages/Product/Product';
 import Home from 'pages/Home/Home';
+import Cart from 'pages/Cart/Cart';
 import NotFound from 'components/404/404';
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/category/:id" element={<Home />} />
-			<Route path="item/:id" element={<Product />} />
+			<Route path="/item/:id" element={<Product />} />
+			<Route path="/cart" element={<Cart />} />
 			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
